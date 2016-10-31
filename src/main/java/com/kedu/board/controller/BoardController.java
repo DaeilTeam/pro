0c3fc013ccd08dfd2bf6760520e0f8e5f959fdc4
염도청 <duaehcjd@naver.com>
2016-10-31 16:46:01 +0900

    받아라 나율몬

---
+++ src/main/java/com/kedu/board/controller/BoardController.java
@@ -0,0 +1,20 @@
+package com.kedu.board.controller;
+
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.kedu.board.service.BoardService;
+
+@Controller
+@RequestMapping("/board/*")
+public class BoardController {
+
+	private static final Logger logger = LoggerFactory.getLogger(BoardController.class);
+	
+	@Inject
+	private BoardService service;
+}
