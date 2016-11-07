9c63eda51683eca3b19f549e8cfda5d0d88495f3
염도청 <duaehcjd@naver.com>
2016-11-07 15:20:08 +0900

    멤버 컨트롤러 수정

--- src/main/java/com/kedu/member/controller/MemberController.java
+++ src/main/java/com/kedu/member/controller/MemberController.java
@@ -1,8 +1,5 @@
 package com.kedu.member.controller;
 
-<<<<<<< HEAD
-=======
-//<<<<<<< HEAD
 //import org.slf4j.Logger;
 //import org.slf4j.LoggerFactory;
 //import org.springframework.stereotype.Controller;
@@ -63,9 +60,6 @@ package com.kedu.member.controller;
 //	@RequestMapping(value="/face",method=RequestMethod.GET)
 //	public void memberF(Model model)throws Exception{
 //	}
-//=======
-//>>>>>>> f8da2118a6c0875972cbda9db5e2b9d6e7a1390f
->>>>>>> 720881c39b2dadc8609733bad0720673abd36cb1
 import java.util.Date;
 
 import javax.inject.Inject;
